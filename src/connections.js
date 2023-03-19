9138fea54490f7524873d741f75312bf778ceebb
Mick L <90425599+Micklitodev@users.noreply.github.com>
2023-03-19 02:45:12 -0400

    0.1.0
    Updated query.sql to include test query, updated schema.sql to have  schema of the database, updated seeds.sql to populate the tables of employee. added a connection.js file to connect to the mySQL db, with dotenv enviornment variables, updated main.js to target the exported function from connection.js and query the database with getDepartments function which awaits a promise to resolve or reject then dynamically maps each dept as a new array and returns that array then departments function uses that array for the inquirer choice options.

---
+++ src/connections.js
@@ -0,0 +1,15 @@
+const mysql2 = require('mysql2');
+const dotenv = require('dotenv');
+dotenv.config(); 
+
+const db = mysql2.createConnection(
+    {
+        host: process.env.HOST_NAME, 
+        user: 'root',
+        password: process.env.PASSWORD_KEY, 
+        database: 'employee_cms'
+    },
+    console.log(`connected to employee_cms db`)
+)
+
+module.exports = db; 
