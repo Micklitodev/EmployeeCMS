3f0fe9603442cb2ef645cc25ec3606ee44961024
Mick L <90425599+Micklitodev@users.noreply.github.com>
2023-03-21 03:11:08 -0400

    1.0.0
    updated employee class in employee.js to have an additional method which updates the role of the selected employee based off of the selected role. All basic functionality completed.

--- db/query.sql
+++ db/query.sql
@@ -1,7 +1,12 @@
-SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name
-AS department, role.salary, manager.first_name, manager.last_name 
-AS manager
-FROM employee
-JOIN role ON employee.role_id = role.id
-JOIN department ON role.department_id = department.id
-LEFT JOIN employee manager ON employee.manager_id = manager.id;
+-- SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name
+-- AS department, role.salary, manager.first_name, manager.last_name 
+-- AS manager
+-- FROM employee
+-- JOIN role ON employee.role_id = role.id
+-- JOIN department ON role.department_id = department.id
+-- LEFT JOIN employee manager ON employee.manager_id = manager.id;
+
+
+
+UPDATE employee SET role_id = 10
+WHERE employee.id = 1;
